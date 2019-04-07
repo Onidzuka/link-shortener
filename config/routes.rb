2c6caed563a2c0877898ef7d96a47f31dc845dca
Nursultan <abdyldaev93@gmail.com>
2019-04-07 00:21:08 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'shortened_urls#new'
+
+  get 's/:unique_key', to: 'link_translators#show', as: :link_translator
+
+  resources :shortened_urls, only: [:new, :create, :show, :edit, :update]
 end
