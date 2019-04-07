4425ea68f730dda079b81dc80b0f4600ca0e1643
Nursultan <abdyldaev93@gmail.com>
2019-04-07 12:50:40 -0700

    refactor

--- app/controllers/link_translators_controller.rb
+++ app/controllers/link_translators_controller.rb
@@ -2,12 +2,18 @@ class LinkTranslatorsController < ApplicationController
   def show
     shortened_url = ShortenedUrl.find_by_unique_key!(params[:unique_key])
 
-    if shortened_url.expires_at.present? && (shortened_url.expires_at < DateTime.current)
+    if link_expired?(shortened_url)
       not_found
     else
       shortened_url.increment!(:views_count, 1)
 
       redirect_to shortened_url.original_url
     end
+  end
+
+  private
+
+  def link_expired?(shortened_url)
+    shortened_url.expires_at.present? && (shortened_url.expires_at < DateTime.current)
   end
 end
