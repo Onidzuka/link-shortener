c44c97838522ec07861e913a713f98dc96271cac
Nursultan <abdyldaev93@gmail.com>
2019-04-07 00:53:41 -0700

    add views_count column to shortened urls table

--- db/migrate/20190407010636_create_shortened_urls.rb
+++ db/migrate/20190407010636_create_shortened_urls.rb
@@ -3,6 +3,7 @@ class CreateShortenedUrls < ActiveRecord::Migration[5.2]
     create_table :shortened_urls do |t|
       t.string :original_url, null: false
       t.string :unique_key, null: false, index: { unique: true }
+      t.integer :views_count, null: false, default: 0
       t.datetime :expires_at
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_04_07_010636) do
   create_table "shortened_urls", force: :cascade do |t|
     t.string "original_url", null: false
     t.string "unique_key", null: false
+    t.integer "views_count", default: 0, null: false
     t.datetime "expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
